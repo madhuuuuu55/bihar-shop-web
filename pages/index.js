43a1e94e7051fcbe4cb3aaf9266b8caeaf04d354
madhuuuuu55 <nocmadhu@gmail.com>
2026-01-05 03:33:38 +0530

    index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#333' }}>BiharBazaar Digital Showroom</h1>
+      <p style={{ color: '#666' }}>Your demo is live! Visit your shop link to see products.</p>
+      <code style={{ background: '#eee', padding: '5px' }}>/raju-vastralaya</code>
+    </div>
+  )
+}
